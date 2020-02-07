63763216c9e6b547c4ce37b8a8ec4799faf95c1c
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-02-07 16:01:11 +0800

    Time Stop

--- 2.7-EMRD.Designer.cs
+++ 2.7-EMRD.Designer.cs
@@ -28,20 +28,315 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSN = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblDepartment = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpStart = new System.Windows.Forms.DateTimePicker();
+            this.dtpCompleted = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.rtxtTechnicianNote = new System.Windows.Forms.RichTextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.cbParts = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.nUDAmount = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nUDAmount)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblDepartment);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.lblName);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.lblSN);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1134, 110);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Selected Asset";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Asset SN: ";
+            // 
+            // lblSN
+            // 
+            this.lblSN.AutoSize = true;
+            this.lblSN.Location = new System.Drawing.Point(95, 45);
+            this.lblSN.Name = "lblSN";
+            this.lblSN.Size = new System.Drawing.Size(46, 17);
+            this.lblSN.TabIndex = 1;
+            this.lblSN.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(311, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Asset Name: ";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(409, 45);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(46, 17);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(758, 45);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Department: ";
+            // 
+            // lblDepartment
+            // 
+            this.lblDepartment.AutoSize = true;
+            this.lblDepartment.Location = new System.Drawing.Point(854, 45);
+            this.lblDepartment.Name = "lblDepartment";
+            this.lblDepartment.Size = new System.Drawing.Size(46, 17);
+            this.lblDepartment.TabIndex = 5;
+            this.lblDepartment.Text = "label6";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rtxtTechnicianNote);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.dtpCompleted);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.dtpStart);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 128);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1134, 268);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Asset EM Report";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Start Date: ";
+            // 
+            // dtpStart
+            // 
+            this.dtpStart.Location = new System.Drawing.Point(98, 36);
+            this.dtpStart.Name = "dtpStart";
+            this.dtpStart.Size = new System.Drawing.Size(170, 22);
+            this.dtpStart.TabIndex = 1;
+            // 
+            // dtpCompleted
+            // 
+            this.dtpCompleted.Location = new System.Drawing.Point(857, 36);
+            this.dtpCompleted.Name = "dtpCompleted";
+            this.dtpCompleted.Size = new System.Drawing.Size(170, 22);
+            this.dtpCompleted.TabIndex = 3;
+            this.dtpCompleted.Value = new System.DateTime(2020, 2, 7, 0, 0, 0, 0);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(742, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 17);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Completed On: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 82);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 17);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Technician Note:";
+            // 
+            // rtxtTechnicianNote
+            // 
+            this.rtxtTechnicianNote.Location = new System.Drawing.Point(21, 102);
+            this.rtxtTechnicianNote.Name = "rtxtTechnicianNote";
+            this.rtxtTechnicianNote.Size = new System.Drawing.Size(1083, 148);
+            this.rtxtTechnicianNote.TabIndex = 5;
+            this.rtxtTechnicianNote.Text = "";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.nUDAmount);
+            this.groupBox3.Controls.Add(this.dataGridView1);
+            this.groupBox3.Controls.Add(this.cbParts);
+            this.groupBox3.Controls.Add(this.btnAdd);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Location = new System.Drawing.Point(13, 403);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(1133, 277);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Replacement Parts";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(391, 695);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 3;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(730, 695);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(49, 33);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 17);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Part Name: ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(690, 33);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(64, 17);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Amount: ";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(990, 30);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(113, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "+ Add to list";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // cbParts
+            // 
+            this.cbParts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbParts.FormattingEnabled = true;
+            this.cbParts.Location = new System.Drawing.Point(138, 30);
+            this.cbParts.Name = "cbParts";
+            this.cbParts.Size = new System.Drawing.Size(417, 24);
+            this.cbParts.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 60);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1121, 211);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // nUDAmount
+            // 
+            this.nUDAmount.DecimalPlaces = 2;
+            this.nUDAmount.Location = new System.Drawing.Point(760, 30);
+            this.nUDAmount.Maximum = new decimal(new int[] {
+            1874919424,
+            2328306,
+            0,
+            0});
+            this.nUDAmount.Name = "nUDAmount";
+            this.nUDAmount.Size = new System.Drawing.Size(120, 22);
+            this.nUDAmount.TabIndex = 6;
             // 
             // EMRD
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1158, 730);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "EMRD";
             this.Text = "EMRD";
             this.Load += new System.EventHandler(this.EMRD_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nUDAmount)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblDepartment;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblSN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox rtxtTechnicianNote;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dtpCompleted;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dtpStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox cbParts;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown nUDAmount;
     }
 }
--- 2.7-EMRD.cs
+++ 2.7-EMRD.cs
@@ -21,7 +21,101 @@ namespace Session2_Redo
 
         private void EMRD_Load(object sender, EventArgs e)
         {
+            dtpCompleted.Text = "";
+            dtpStart.Text = "";
+            using (var context = new Session2Entities())
+            {
+                var getParts = (from x in context.Parts
+                                select x.Name);
+                foreach (var item in getParts)
+                {
+                    cbParts.Items.Add(item);
+                }
 
+                var getAssetInfo = (from x in context.EmergencyMaintenances
+                                    where x.ID == _emID
+                                    select x).First();
+                lblSN.Text = getAssetInfo.Asset.AssetSN;
+                lblDepartment.Text = getAssetInfo.Asset.DepartmentLocation.Department.Name;
+                lblName.Text = getAssetInfo.Asset.AssetName;
+                LoadGrid();
+            }
+        }
+        private void LoadGrid()
+        {
+            dataGridView1.ColumnCount = 3;
+            dataGridView1.Columns[0].Name = "Part Name";
+            dataGridView1.Columns[1].Name = "Amount";
+            dataGridView1.Columns[2].Name = "Part ID";
+            dataGridView1.Columns[2].Visible = false;
+            DataGridViewLinkColumn linkCell = new DataGridViewLinkColumn()
+            {
+                LinkColor = Color.Blue,
+                Name = "Action",
+                Text = "Remove",
+                UseColumnTextForLinkValue = true
+            };
+            dataGridView1.Columns.Add(linkCell);
+            dataGridView1.Rows.Clear();
+            using (var context = new Session2Entities())
+            {
+                var getPartsChanged = (from x in context.ChangedParts
+                                       where x.EmergencyMaintenanceID == _emID
+                                       select x);
+                foreach (var item in getPartsChanged)
+                {
+                    var rows = new List<string>()
+                    {
+                        item.Part.Name, item.Amount.ToString(), item.PartID.ToString()
+                    };
+                    dataGridView1.Rows.Add(rows.ToArray());
+                }
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (var context = new Session2Entities())
+            {
+
+                var checkPart = (from x in context.ChangedParts
+                                 where x.Part.Name == cbParts.SelectedItem.ToString()
+                                 orderby x.EmergencyMaintenance.EMEndDate descending
+                                 select new { timeLeft = DateTime.Now - x.EmergencyMaintenance.EMEndDate }).FirstOrDefault();
+
+                foreach (DataGridViewRow item in dataGridView1.Rows)
+                {
+                    if (item.Cells[0].Value.ToString() == cbParts.SelectedItem.ToString())
+                    {
+                        MessageBox.Show("Cannot add duplicate parts! Please remove and add again!", "Duplicate detected",
+                            MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
+                }
+                if (cbParts.SelectedItem == null)
+                {
+                    MessageBox.Show("Please select a part!", "No part detected",
+                            MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                if (nUDAmount.Value == 0)
+                {
+                    MessageBox.Show("Please choose a value more than 0!", "Invalid amount detected",
+                            MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else if (checkPart.timeLeft.Value.TotalDays < context.Parts.Where(x => x.Name == cbParts.SelectedItem.ToString()).Select(x => x.EffectiveLife).First())
+                {
+                    var dl = MessageBox.Show("Are you sure you want to add? Effective life is not over", "Add to list",
+                            MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (dl == DialogResult.Yes)
+                    {
+
+                    }
+                }
+            }
         }
     }
 }
